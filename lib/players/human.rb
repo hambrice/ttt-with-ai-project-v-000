471e621c9e7fd4fc549d95517b109e2dcbd67d2b
Brad Hambrice <hambrice@uab.edu>
2018-01-14 20:13:39 +0000

    automatically backed up by learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,10 +1,15 @@
-require 'pry'
+log10require 'pry'
 
 module Players
   class Human < Player
   
     def move(board)
     input = gets.strip  
+    if board.taken?(input)
+      puts "That spot is taken! Please pick another."
+      self.move(board)
+    end
+    input
     end
     
   end
